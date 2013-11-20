cfbcf80552c6010d6b3fdfcb7bc06580376f97cf
Salzmann <kirill.salzmann@web.de>
2013-11-20 15:11:50 +0100

    Options and Credits Prototype finalized

---
+++ Assets/Scripts/FixCreditsReset.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class FixCreditsReset : MonoBehaviour
+{
+
+	public dfButton AbortButton;
+    public dfTweenVector3 ScrollingCredits;
+    public dfTweenVector3 AbortButtonTween;
+    public Vector3 XEndPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		foreach (dfButton button in FindObjectsOfType<dfButton>().Where(button => button.name == "Button_EndCredits"))
+		{
+			AbortButton = button;
+		}
+        ScrollingCredits = GetComponent<dfTweenVector3>();
+        foreach (dfTweenVector3 tween in AbortButton.GetComponents<dfTweenVector3>().Where(tween => tween.TweenName == "BlendOut"))
+        {
+            XEndPos = tween.EndValue;
+            AbortButtonTween = tween;
+        }
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (!ScrollingCredits.IsPlaying) return;
+	    if (!AbortButtonTween.IsPlaying && AbortButton.Position != XEndPos) return;
+
+	    ScrollingCredits.Stop();
+	    ScrollingCredits.Reset();
+	}
+}
---
+++ Assets/Scripts/ResetTweenPosition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetTweenPosition : MonoBehaviour
+{
+    public void resetTween()
+    {
+        GetComponent<dfLabel>().Position = GetComponent<dfTweenVector3>().StartValue;
+    }
+}
