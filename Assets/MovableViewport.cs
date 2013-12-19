a630b69160dae08ffc90eaaeecabbbbc185938a5
Anton Licht <anton.licht@gmx.de>
2013-12-19 23:57:47 +0100

    Implement Scrolling for orthografic viewports

---
+++ Assets/MovableViewport.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MovableViewport : MonoBehaviour
+{
+    public float phase;
+    public bool pinned;
+	void Start ()
+	{
+	    
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+        Debug.Log(camera.orthographicSize+" "+camera.fieldOfView);
+	    camera.rect = new Rect(phase,0,1,1);
+        float width = ((float)Screen.width/Screen.height)*camera.orthographicSize;
+        Vector3 camoffset = new Vector3((pinned?-1f:1f)*(camera.rect.x * width), 0, 0);
+		Matrix4x4 m  = Matrix4x4.TRS (camoffset, Quaternion.identity, new Vector3 (1,1,-1));
+		camera.worldToCameraMatrix = m * transform.worldToLocalMatrix;
+	}
+}
---
+++ Assets/ScrollingRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingRenderer : MonoBehaviour
+{
+    private MovableViewport _gameCam;
+    private MovableViewport _guiCam;
+	// Use this for initialization
+	void Awake ()
+	{
+        _gameCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<MovableViewport>();
+        _guiCam = GameObject.FindGameObjectWithTag("GUICamera").GetComponent<MovableViewport>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float phase = (Mathf.Sin(Time.time) + 1f)/2f;
+	    _gameCam.phase = phase;
+	    _guiCam.phase = phase - 1;
+	}
+}
