68fb73b6fabf95d3ae7738454a3945bb6d14a2e1
Anton Licht <anton.licht@gmx.de>
2013-12-23 15:58:30 +0100

    Movable Viewport for perspective cam

--- Assets/MovableViewport.cs
+++ Assets/MovableViewport.cs
@@ -4,19 +4,22 @@ public class MovableViewport : MonoBehaviour
 {
     public float phase;
     public bool pinned;
-	void Start ()
-	{
-	    
-	}
 	
 	// Update is called once per frame
 	void LateUpdate ()
 	{
-        Debug.Log(camera.orthographicSize+" "+camera.fieldOfView);
-	    camera.rect = new Rect(phase,0,1,1);
-        float width = ((float)Screen.width/Screen.height)*camera.orthographicSize;
-        Vector3 camoffset = new Vector3((pinned?-1f:1f)*(camera.rect.x * width), 0, 0);
-		Matrix4x4 m  = Matrix4x4.TRS (camoffset, Quaternion.identity, new Vector3 (1,1,-1));
-		camera.worldToCameraMatrix = m * transform.worldToLocalMatrix;
+	    camera.rect = new Rect(phase, 0, 1, 1);
+	    if (camera.rect.x >= 1 || camera.rect.x < -1)
+	        return;
+	    float translation;
+        translation = (!pinned ^ camera.rect.x >= 0 ? 1f : -1f) * (-1f + (1f / (1f - Mathf.Abs(camera.rect.x))));
+        Vector3 camoffset = new Vector3(translation, 0, 0);
+	    Matrix4x4 m = Matrix4x4.TRS(camoffset, Quaternion.identity, new Vector3(1, 1, 1));
+        camera.ResetProjectionMatrix();       
+	    camera.projectionMatrix = m*camera.projectionMatrix;
+
+
 	}
+
 }
+
