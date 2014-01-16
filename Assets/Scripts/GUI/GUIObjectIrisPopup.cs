ecf1f9444b8fce7821776aa91115dad2a4242681
Anton Licht <anton.licht@gmx.de>
2014-01-16 22:57:49 +0100

    Add Repeat Button to Iris

--- Assets/Scripts/GUI/GUIObjectIrisPopup.cs
+++ Assets/Scripts/GUI/GUIObjectIrisPopup.cs
@@ -1,18 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class GUIObjectIrisPopup : MonoBehaviour 
 {
     private const string PopupStr = "sprite_popup";
     private const string VisualizerStr = "panel_visualizer";
+    private const string RepeatButtonStr = "button_repeat";
     private const string TextLabelStr = "label_text";
 
+    private const float Delay = 1f;
+
     private string _textkeyText = "blindtext";
-    public string Audio = "";
+
+    public string Audio
+    {
+        get 
+        { 
+            if (_audio && _audio.clip) return _audio.clip.name;
+            return "null";
+        }
+        set
+        {
+            _audio.clip = Localization.GetSound(value);
+            Debug.Log(_audio.clip);
+        }
+    }
 
     private dfLabel _textLabel;
+    private dfButton _repeat;
     private AudioSource _messageSound;
     private AudioSource _audio;
+    private bool _repeatEnabled = false;
+
+    public event Action ShowRepeat;
+    public event Action HideRepeat;
 
     public string Text
     {
@@ -29,28 +51,62 @@ public class GUIObjectIrisPopup : MonoBehaviour
     void Awake()
     {
         _messageSound = GetComponent<AudioSource>();
+        _messageSound.Play();
+
         _audio = gameObject.AddComponent<AudioSource>();
 
-        GameObject obj = transform.FindChild(PopupStr).FindChild(TextLabelStr).gameObject;
-        if (obj)
-            _textLabel = obj.GetComponent<dfLabel>();
+        Transform popUpTrnsf = transform.FindChild(PopupStr);
 
-        obj = transform.FindChild(PopupStr).FindChild(VisualizerStr).gameObject;
-        if (obj)
-            obj.GetComponent<GUIObjectVisualizer>().Audio = _audio;
+        if (popUpTrnsf != null)
+        {
+            GameObject obj = popUpTrnsf.FindChild(TextLabelStr).gameObject;
+            if (obj)
+                _textLabel = obj.GetComponent<dfLabel>();
+
+            obj = popUpTrnsf.FindChild(VisualizerStr).gameObject;
+            if (obj)
+                obj.GetComponent<GUIObjectVisualizer>().Audio = _audio;
+
+
+            obj = popUpTrnsf.FindChild(RepeatButtonStr).gameObject;
+            if (obj)
+            {
+                _repeat = obj.GetComponent<dfButton>();
+                _repeat.Disable();
+                _repeat.Click += (dfControl control, dfMouseEventArgs mouseEvent) =>
+                {
+                    if (mouseEvent.Used) return;
+                    mouseEvent.Use();
+                    PlaySound();
+                    _repeat.Disable();
+                    if (HideRepeat != null)
+                        HideRepeat();
+                };
+            }
+        }
+        
 
     }
 
     void Update()
     {
-
+        if (_audio == null || _audio.clip == null)
+            return;
+        if (!_repeatEnabled && _audio.isPlaying)
+            _repeatEnabled = true;
+        
+        else if (_repeatEnabled && !_audio.isPlaying && !_repeat.IsEnabled)
+        {
+            _repeat.Enable();
+            if (ShowRepeat != null)
+                ShowRepeat();
+        }
     }
 
     public void PlaySound()
     {
-        _messageSound.Play();
-        _audio.clip = Localization.GetSound(Audio);
-        _audio.PlayDelayed(1f);
+        
+        _audio.PlayDelayed(Delay);
         //StartCoroutine(LoadAndPlay());
     }
 
