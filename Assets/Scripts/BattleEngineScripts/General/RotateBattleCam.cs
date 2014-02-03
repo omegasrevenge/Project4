1ea726f4af9a57fb8cee69b109c0b32083494fd8
omegasrevenge <kirill.salzmann@web.de>
2014-02-03 12:04:31 +0100

    BattleEngine: Camera Fix

--- Assets/Scripts/BattleEngineScripts/General/RotateBattleCam.cs
+++ Assets/Scripts/BattleEngineScripts/General/RotateBattleCam.cs
@@ -11,12 +11,15 @@ public class RotateBattleCam : MonoBehaviour
 
 	[HideInInspector]
 	public bool DoRotation = false;
+
+    private float positiveOffset = 2f;
+    private float negativeOffset = 0.4f;
 	
 	public bool CameraInAPosition
 	{
 		get
 		{
-			return (Mathf.Abs(transform.localEulerAngles.y) >= -0.4f) && (Mathf.Abs(transform.localEulerAngles.y) <= 2f);
+            return (Mathf.Abs(transform.localEulerAngles.y) >= -negativeOffset) && (Mathf.Abs(transform.localEulerAngles.y) <= positiveOffset);
 		}
 	}
 
@@ -24,7 +27,7 @@ public class RotateBattleCam : MonoBehaviour
 	{
 		get
 		{
-			return (Mathf.Abs(transform.localEulerAngles.y) >= 178f) && (Mathf.Abs(transform.localEulerAngles.y) <= 180.4f);
+            return (Mathf.Abs(transform.localEulerAngles.y) >= 180f - positiveOffset) && (Mathf.Abs(transform.localEulerAngles.y) <= 180f + negativeOffset);
 		}
 	}
 
@@ -36,32 +39,18 @@ public class RotateBattleCam : MonoBehaviour
 		if(!DoRotation) return;
 
 		if(CameraInAPosition)
-		{
 			_currentDir = To.B;
-		}
 
 		if(CameraInBPosition)
-		{
 			_currentDir = To.A;
-		}
 
 		if(_currentDir == To.B)
-		{
-			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 
-			                                         transform.localEulerAngles.y-(RotationSpeed*Time.deltaTime), 
-			                                         transform.localEulerAngles.z);
-		}
+			transform.localEulerAngles -= new Vector3(0, RotationSpeed * Time.deltaTime, 0);
 		else
-		{
-			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 
-			                                         transform.localEulerAngles.y+(RotationSpeed*Time.deltaTime), 
-			                                         transform.localEulerAngles.z);
-		}
+            transform.localEulerAngles += new Vector3(0, RotationSpeed * Time.deltaTime, 0);
 
 		if(!CameraInAPosition && !CameraInBPosition)
-		{
 			_canStopRotation = true;
-		}
 
 		if(_canStopRotation && (CameraInAPosition || CameraInBPosition))
 		{
