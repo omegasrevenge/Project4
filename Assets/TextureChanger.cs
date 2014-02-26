06e4fd2d1ba8a9e88720d89f9a4783028a14a0bb
Maximilian Frank <Maximilian.F1@gmx.de>
2014-02-26 17:06:14 +0100

    textures, sounds, base

---
+++ Assets/TextureChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureChanger : MonoBehaviour
+{
+    public Texture Vengea;
+    public Texture VengeaNormal;
+    public Texture Nce;  
+    public Texture NceNormal;
+
+	void Start ()
+	{
+	    Material material = GetComponent<MeshRenderer>().material;
+        if (GameManager.Singleton.Player.CurrentFaction == Player.Faction.VENGEA)
+	    {
+            material.SetTexture("_MainTex", Vengea);
+            material.SetTexture("_BumpMap", VengeaNormal);
+	    }
+	    else
+	    {
+            material.SetTexture("_MainTex", Nce);
+            material.SetTexture("_BumpMap", NceNormal);
+	    }
+	}
+}
