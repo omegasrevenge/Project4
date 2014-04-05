e8d37494ee85fba69d410e5534a2ead908d35ae1
Anton Licht <anton.licht@gmx.de>
2014-04-05 19:01:41 +0200

    add hide menu button

---
+++ Assets/Scripts/View/GUIObjectOpenMenuButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIObjectOpenMenuButton : MonoBehaviour
+{
+    private dfControl _control;
+	// Use this for initialization
+	void Start ()
+	{
+	    _control = GetComponent<dfControl>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(GameManager.Singleton.CurrentGameMode != GameManager.GameMode.Map)
+	        _control.Hide();
+        else
+        {
+            _control.Show();
+        }
+	}
+}
